8b70ff8586111d4c506b0c996937d987c890a13a
Steven <decleene.steven@gmail.com>
2023-03-11 20:45:36 +0100

    Merge bean tests from branch 01 to main

---
+++ src/test/java/com/stivizu/tutorial/spring/scenario_04/service/JavaService.java
@@ -0,0 +1,12 @@
+package com.stivizu.tutorial.spring.scenario_04.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JavaService {
+
+    public void sayHello() {
+        log.info("Hello!");
+    }
+
+}
---
+++ src/test/java/com/stivizu/tutorial/spring/scenario_05/configuration/BeanConfiguration.java
@@ -0,0 +1,24 @@
+package com.stivizu.tutorial.spring.scenario_05.configuration;
+
+import com.stivizu.tutorial.spring.scenario_05.service.DependentJavaService;
+import com.stivizu.tutorial.spring.scenario_05.service.JavaService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public JavaService javaService() {
+        log.info("---> Creating a new JavaService instance!");
+        return new JavaService();
+    }
+
+    @Bean
+    public DependentJavaService dependentJavaService() {
+        return new DependentJavaService(javaService());
+    }
+
+}
