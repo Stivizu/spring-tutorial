58565e770414b1520335bd40be6e19325b272a6e
Steven de Cleene <decleene.steven@gmali.com>
2023-03-13 19:05:44 +0100

    Minor fix

--- src/main/java/com/stivizu/tutorial/spring/model/entity/StudentEntityMapper.java
+++ src/main/java/com/stivizu/tutorial/spring/model/entity/StudentEntityMapper.java
@@ -3,11 +3,13 @@ package com.stivizu.tutorial.spring.model.entity;
 import com.stivizu.tutorial.spring.model.Student;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface StudentEntityMapper {
 
+    @Mapping(target = "active", constant = "true")
     StudentEntity toEntity(Student student);
 
     Student toDomain(StudentEntity studentEntity);
@@ -15,7 +17,7 @@ public interface StudentEntityMapper {
     /*
         This method is used by MapStruct to do something after the mapping has been done.
          Rather than adding a @Mappings annotation, this has way better flexibility for what we want to achieve.
-         Do not that this might have some issues when combined with the Lombok builder.
+         Do note that this might have some issues when combined with the Lombok builder.
          Which is the main reason why in this phase we only kept the @Builder annotation on the domain objects.
      */
     @AfterMapping
